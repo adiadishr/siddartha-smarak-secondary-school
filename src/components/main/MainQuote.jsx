5f81610d4a074df7ec42547034f839c859a0c843
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-05-10 09:35:23 +0545

    change toq uote section in height

--- src/components/main/MainQuote.jsx
+++ src/components/main/MainQuote.jsx
@@ -3,7 +3,7 @@ import { ImQuotesLeft } from "react-icons/im";
 
 const MainQuote = () => {
   return (
-    <div className='flex w-full px-[7.5%] items-center justify-center min-h-screen'>
+    <div className='flex w-full px-[7.5%] items-center justify-center md:min-h-screen min-h-[70vh]'>
       <div className='flex flex-col items-center justify-center w-full gap-12'>
         <div className='text-4xl text-orange-500'>
           <ImQuotesLeft />
