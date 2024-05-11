75228d199c5c1043fe220370e515ca0d897541af
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-05-11 14:50:51 +0545

    contact form

--- src/components/global/AdmissionDrawer.jsx
+++ src/components/global/AdmissionDrawer.jsx
@@ -28,19 +28,60 @@ const AdmissionDrawer = ({ place }) => {
                         </div>
                     </DrawerTrigger> :
                     <DrawerTrigger id='admissionButton' className='px-4 py-[10px] text-white bg-orange-500 border-2 border-transparent rounded-xl hover:opacity-80 duration-300 flex items-center justify-center'><PiStudentFill className="mr-2 size-6" />Admissions</DrawerTrigger>
-            } 
-            <DrawerContent className='md:px-[5%] max-h-[90vh] overflow-y-scroll'>
-                <DrawerHeader>
+            }
+            <DrawerContent className='px-[5%] h-screen overflow-y-hidden'>
+                <DrawerHeader className='px-0 mx-0'>
                     <DrawerTitle className='text-2xl/[110%] text-sky-800 font-merriwether'>Interested in building your future with us?</DrawerTitle>
                     <DrawerDescription className='text-base font-manrope text-stone-400'>Fill up this form, and we'll contact you ASAP regarding admission and joining us!</DrawerDescription>
                 </DrawerHeader>
+                <form className="flex flex-col justify-between h-full">
+                    <hr />
+                    <section className="flex flex-col gap-4 mt-4">
+                        <div className="flex flex-col justify-between gap-4 md:gap-8 md:flex-row">
+                            <div className="flex flex-col w-full">
+                                <p className="flex pb-2 text-lg font-merriwether text-sky-900">Name of Guardian:</p>
+                                <input placeholder="Enter Guardian's Name" className="w-full px-2 py-2 border-2 font-manrope text-neutral-700" type="text" />
+                            </div>
+                            <div className="flex flex-col w-full">
+                                <p className="flex pb-2 text-lg font-merriwether text-sky-900">Name of Student:</p>
+                                <input placeholder="Enter Student's Name" className="w-full px-2 py-2 border-2 font-manrope text-neutral-700" type="text" />
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-between gap-4 md:gap-8 md:flex-row">
+                            <div className="flex flex-col w-full">
+                                <p className="flex pb-2 text-lg font-merriwether text-sky-900">Class:</p>
+                                <select className="w-full px-2 py-2 border-2 font-manrope text-neutral-700">
+                                    <option value="" disabled selected>Choose your class:</option>
+                                    <option value="Class 1">Class 1</option>
+                                    <option value="Class 2">Class 2</option>
+                                    <option value="Class 3">Class 3</option>
+                                    <option value="Class 4">Class 4</option>
+                                    <option value="Class 4">Class 4</option>
+                                    <option value="Class 5">Class 5</option>
+                                    <option value="Class 6">Class 6</option>
+                                    <option value="Class 6">Class 6</option>
+                                    <option value="Class 7">Class 7</option>
+                                    <option value="Class 8">Class 8</option>
+                                    <option value="Class 9">Class 9</option>
+                                    <option value="Class 10">Class 10</option>
+                                </select>
+                            </div>
+                            <div className="flex flex-col w-full">
+                                <p className="flex pb-2 text-lg font-merriwether text-sky-900">Contact Details:</p>
+                                <input className="w-full px-2 py-2 border-2 font-manrope text-neutral-700" placeholder="Enter your phone number" type="text" />
+                            </div>
+                        </div>
+                        <textarea rows={5} placeholder="Any other details?" className="w-full px-2 py-2 mt-2 border-2 font-manrope">
 
-                <DrawerFooter>
-                    <Button className=' bg-sky-800/80 hover:bg-sky-800/60'>Submit</Button>
-                    <DrawerClose className="flex items-center ">
-                        <Button className="w-full" variant="outline">Cancel</Button>
-                    </DrawerClose>
-                </DrawerFooter>
+                        </textarea>
+                    </section>
+                    <DrawerFooter className='px-0 mx-0'>
+                        <input type="submit" className='py-2 font-semibold text-white rounded-md cursor-pointer bg-sky-800/80 hover:bg-sky-800/60' />
+                        <DrawerClose className="flex items-center ">
+                            <Button className="w-full" variant="outline">Cancel</Button>
+                        </DrawerClose>
+                    </DrawerFooter>
+                </form>
             </DrawerContent>
         </Drawer>
     )
